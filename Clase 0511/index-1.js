dfc0733239953e7ff804f02f5e760f0e16e48f6e
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-11-05 21:02:50 -0300

    Clase 05/11

---
+++ Clase 0511/index-1.js
@@ -0,0 +1,61 @@
+const arraicito = [1, 8, 2, 5, 6, 2]
+
+const arrayMayoresA3 = arraicito.filter( (elemento) => elemento > 3 )
+
+console.log(arrayMayoresA3)
+
+
+
+
+
+
+
+
+
+// const arrayDoble = arraicito.map( (elemento) => elemento * 2)
+
+// const arrayDoble2 = arraicito.map( function(elemento) {
+//     return elemento * 2
+// } )
+
+// console.log(arrayDoble)
+
+// console.log(arraicito)
+
+
+
+
+// arraicito.forEach((numero, indice)=>{
+//     console.log("elemento: ", indice, "valor: ", numero)
+// })
+
+
+// const funcionanonima = function (elemento) {
+// 	console.log("elemento a mostrar: ", elemento)
+// }
+
+// const funcionFlecha = (elemento) => {
+// 	console.log("elemento a mostrar: ", elemento)
+// }
+
+// arraicito.forEach(function (){
+
+// })
+
+// arraicito.forEach(function (elemento) {
+// 	console.log("elemento a mostrar: ", elemento)
+// })
+
+// arraicito.forEach(  (elemento) => {
+// 	console.log("elemento a mostrar: ", elemento)
+// }  )
+
+// for (let contador = 0; contador < 5; contador++) {
+
+//     mostrarElementoArray(arraicito[contador])
+
+// }
+
+// function mostrarElementoArray(elemento){
+//     console.log("elemento a mostrar: ", elemento)
+// }
---
+++ Clase 0511/index-2.js
@@ -0,0 +1,6 @@
+
+const li = document.createElement('li')
+const ul = document.querySelector('ul')
+li.innerText = "Hola desde js"
+
+ul.appendChild(li)
