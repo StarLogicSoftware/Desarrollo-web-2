3bb057cd7dbfbb2b496738d55bd9a5442cd45b24
Starlogic - Nicolás Fumo <starlogicsoftware@gmail.com>
2024-11-26 21:15:54 -0300

    Clase 26/11

--- Clase 2611/promesas.js
+++ Clase 2611/promesas.js
@@ -1,4 +1,15 @@
 
+const lista = document.querySelector('#lista')
 
 fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => console.log(response))
+      .then(response => response.json())
+      .then(json => {
+        json.forEach(post=> {
+            const {title, body} = post
+           
+            const li = document.createElement('li')
+            li.textContent = title
+
+            lista.appendChild(li)
+        });
+      })
